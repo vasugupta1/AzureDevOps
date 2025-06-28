bf02d12d427e37d0842f4be8ca9ab705b3e416c4
Vasu Gupta <vasugupta1@gmail.com>
2025-06-28 14:40:15 +0100

    moved into modules as that is much eaiser to remove resouces as I go

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource "azurerm_resource_group" "az-204" {
     name = var.resouce_group_name
     location = var.location
 }
+
+module "azure_function_app" {
+    source = "./modules/az_function"
+    resouce_group_name = var.resouce_group_name
+    location = var.location
+    function_app_name = var.function_app_name
+}
--- modules/az_function/main.tf
+++ modules/az_function/main.tf
@@ -1,23 +1,23 @@
 resource "azurerm_storage_account" "azure_function_storage_account" {
     name = "${var.function_app_name}sa"
-    resource_group_name = azurerm_resource_group.az-204.name
-    location = azurerm_resource_group.az-204.location
+    resource_group_name = var.resouce_group_name
+    location = var.location
     account_tier = "Standard"
     account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "azure_function_service_plan" {
     name = "${var.function_app_name}sp"
-    resource_group_name = azurerm_resource_group.az-204.name
-    location = azurerm_resource_group.az-204.location
+    resource_group_name = var.resouce_group_name
+    location = var.location
     os_type = "Linux"
     sku_name = "B1"
 }
 
 resource "azurerm_linux_function_app" "azure_linux_function" {
     name = "${var.function_app_name}${random_string.suffix.result}"
-    resource_group_name = azurerm_resource_group.az-204.name
-    location = azurerm_resource_group.az-204.location
+    resource_group_name = var.resouce_group_name
+    location = var.location
     site_config {
     }
     service_plan_id = azurerm_service_plan.azure_function_service_plan.id
